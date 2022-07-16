93cae1c2bb5f8a52133381d1bcd2c61218646c6a
Tomas-Paoletti <tomiagustin.tp@gmail.com>
2022-07-15 21:39:20 -0300

    schema and routes

--- NODE-DESAROLLO -UTIL/proyecto api/src/config/express.js
+++ NODE-DESAROLLO -UTIL/proyecto api/src/config/express.js
@@ -1,7 +1,12 @@
-import express from 'express';
+import userRouter from '#Routes/user.routes.js';
+import express, { application } from 'express';
 
 const App = express();
 
 // todo modlewares, routes etx....
+App.use(express.json())
+
+//routes
+application.use('/user', userRouter)
 
 export default App;
